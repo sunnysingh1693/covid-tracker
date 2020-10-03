1439350b844b923126aecc267dab4b4c903f6383
Sunny Singh <sunnysingh@MacBook-Pro.local>
2020-10-04 01:02:07 +0530

    cards data display and style completed

--- src/App.js
+++ src/App.js
@@ -5,16 +5,21 @@ import styles from './App.module.css';
 import { fetchData } from './api';
 
 class App extends Component {
+  state = {
+    data: {}
+  }
 
   async componentDidMount() {
-    const data = await fetchData();
-    console.log(data);
+    const fetchedData = await fetchData();
+    console.log(fetchedData)
+    this.setState({ data: fetchedData });
   }
 
   render() {
+    const { data } = this.state;
     return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data={data} />
         <CountryPicker />
         <Chart />
       </div>
--- src/api/index.js
+++ src/api/index.js
@@ -4,9 +4,9 @@ const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async () => {
   try {
-    const { data: { confirmed, recovered, deaths, lastUpdated } } = await axios.get(url);
+    const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
 
-    return { confirmed, recovered, deaths, lastUpdated };
+    return { confirmed, recovered, deaths, lastUpdate };
   } catch (error) {
 
   }
